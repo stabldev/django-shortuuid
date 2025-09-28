63ca1543313755d7b22615e07aa59993e57d1ef0
stabldev <114811070+stabldev@users.noreply.github.com>
2025-09-28 22:44:50 +0530

    chore: update django_example

--- django_example/config/wsgi.py
+++ django_example/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_example project.
+WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_example.settings')
-
+_ = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 application = get_wsgi_application()
